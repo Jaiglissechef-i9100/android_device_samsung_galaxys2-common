932ddef2bea3aca79ea59362e525d50601736805
varund7726 <date.varun123@gmail.com>
2015-07-24 01:08:30 +0530

    Derp

--- common.mk
+++ common.mk
@@ -112,10 +112,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
-<<<<<<< HEAD
-    
-=======
->>>>>>> 8feed3c... galaxys2-common: don't copy ffmpeg config anymore
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
